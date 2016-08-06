da998f707de9a546a6ae16eea230fbb5bcd4b94a
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2016-08-06 19:02:54 +0100

    remove patch for npm 2

--- config/babel-test-require.js
+++ config/babel-test-require.js
@@ -1,13 +1,5 @@
 var path = require('path');
 
-// XX: patch for npm 2
-// baker-scripts will have all the deps installed within
-// local node_modules directory so we need to provide the
-// outside app with a way to see through it
-require('app-module-path').addPath(
-  path.resolve(__dirname, '../node_modules')
-);
-
 require('babel-register')({
   presets: ['babel-preset-es2015'].map(require.resolve),
 });
